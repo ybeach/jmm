09ba103deec97202014c959bc16f4c7e45bd8ec8
ybeach <taizo.yamada@gmail.com>
2024-11-25 15:10:19 +0900

    temporary

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  srcDir: 'src/',
+  css:['@/assets/css/style.css'],
+  app: {
+    head:{
+      title: 'JSPS科研費基盤研究（S）24H00011',
+      charset: 'utf-8',
+      meta:[
+        {
+          name: 'JSPS科研費基盤研究（S）24H00011 史料データセンシングに基づく日本列島記憶継承モデルの確立',
+          content: 'JSPS科研費基盤研究（S）(24H00011)「史料データセンシングに基づく日本列島記憶継承モデルの確立」の概要・活動をお知らせします',
+        }
+      ],
+      htmlAttrs:{
+        lang: 'ja',
+      },
+    }
+  }
 })
---
+++ src/app.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { NuxtLayout, NuxtPage } from '#build/components';
+</script>
+
+<template>
+  <NuxtLayout>
+    <!--<NuxtRouteAnnouncer />-->
+    <!--<NuxtWelcome />-->
+    <NuxtPage />
+  </NuxtLayout>
+</template>
